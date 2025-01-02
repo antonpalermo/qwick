445a55ea32492f4b10c6608fa6304847c54002c5
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-01-02 15:06:11 +0800

    setup shadcn

--- apps/admin-dashboard/src/components/app.tsx
+++ apps/admin-dashboard/src/components/app.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
     <div>
       <h1>App</h1>
+      <Button>Sample</Button>
     </div>
   );
 }
--- apps/admin-dashboard/src/main.tsx
+++ apps/admin-dashboard/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from './components/app.tsx'
+import App from "./components/app.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "./globals.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
