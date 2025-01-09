949bb81c2f5a0a1ef074dc0b24ecef321e4bda1e
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-01-09 23:07:08 +0800

    fix: serialize errors

--- apps/backend-rest/src/strategies/google-oauth20.strategy.ts
+++ apps/backend-rest/src/strategies/google-oauth20.strategy.ts
@@ -1,19 +1,28 @@
 import passport from "passport";
 import { Strategy as Google0Auth20Strategy } from "passport-google-oauth20";
 
+passport.serializeUser((id, done) => {
+  done(null, id);
+});
+
+passport.deserializeUser((id, done) => {
+  done(null, { user: null });
+});
+
 export default passport.use(
   new Google0Auth20Strategy(
     {
       clientID: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-      callbackURL: "/api/auth/callback/google"
+      callbackURL: "/api/auth/callback/google",
+      state: true
     },
     (accessToken, refreshToken, profile, done) => {
       console.log("accessToken", accessToken);
       console.log("refreshToken", refreshToken);
       console.log("profile", profile);
 
-      return done(null, profile);
+      return done(null, profile.id);
     }
   )
 );
