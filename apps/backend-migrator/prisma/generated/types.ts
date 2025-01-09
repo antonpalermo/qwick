b6895380d8f618a0b9d03cf8578cd5ee97b5415a
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-01-09 23:38:10 +0800

    added user schema

--- apps/backend-migrator/prisma/generated/types.ts
+++ apps/backend-migrator/prisma/generated/types.ts
@@ -5,11 +5,29 @@ export type Generated<T> =
     : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
+export type Account = {
+  id: Generated<string>;
+  account_id: string;
+  access_token: string | null;
+  provider: string;
+  userId: string;
+};
 export type Inventory = {
   id: string;
   createdDate: Generated<Timestamp>;
   updatedDate: Timestamp;
 };
+export type User = {
+  id: Generated<string>;
+  name: string;
+  image: string;
+  email: string;
+  email_verified: Generated<boolean>;
+  created: Generated<Timestamp>;
+  updated: Timestamp;
+};
 export type DB = {
+  accounts: Account;
   inventory: Inventory;
+  users: User;
 };
