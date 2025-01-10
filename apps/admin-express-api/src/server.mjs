428d1d9f1abc147e44ef32bc4d693f4a2c459cfe
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-01-11 00:21:29 +0800

    initialize passport js

--- apps/admin-express-api/src/server.mjs
+++ apps/admin-express-api/src/server.mjs
@@ -1,10 +1,28 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import expressSession from "express-session";
+import passport from "passport";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ server: "ok" });
-});
+const corsOptions = {
+  origin: ["http://localhost:5731"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser(process.env.SECRET_KEYID));
+app.use(
+  expressSession({
+    secret: process.env.SECRET_KEYID,
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.listen(8080, () => {
   console.log(`server listening on http://localhost:8080`);
