51c4b9752aaa9d7674bc6da5413639334902e4ff
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-01-14 23:38:10 +0800

    refac: update signin redirects

--- apps/admin-express-api/src/routes/auth.route.mjs
+++ apps/admin-express-api/src/routes/auth.route.mjs
@@ -1,6 +1,8 @@
 import express from "express";
 import passport from "passport";
 
+import Logger, { Namespace } from "../utils/logger.mjs";
+
 const router = express.Router({
   strict: true
 });
@@ -9,9 +11,10 @@ router.post(
   "/login/email",
   passport.authenticate("magiclink", {
     action: "requestToken",
-    failureRedirect: "/login"
+    failureRedirect: "http://localhost:5173/auth/signin"
   }),
   (req, res) => {
+    Logger(Namespace.AUTH, `magic link sent to ${req.body.email}`);
     return res.status(200).json("email successfully sent");
   }
 );
@@ -20,7 +23,7 @@ router.get(
   "/login/email/verify",
   passport.authenticate("magiclink", { action: "acceptToken" }),
   (req, res) => {
-    return res.redirect("/");
+    return res.redirect("http://localhost:5173/");
   }
 );
 
