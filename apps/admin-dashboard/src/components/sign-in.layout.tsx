400a1aa98024b182fb05542dcfdfab6caf0e6000
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-01-15 00:14:25 +0800

    feat: added magic-link flow in frontend

---
+++ apps/admin-dashboard/src/components/sign-in.layout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router";
+
+export default function SignInLayout() {
+  return (
+    <div>
+      <h1>SignIn Layout</h1>
+      <Outlet />
+    </div>
+  );
+}
--- apps/admin-dashboard/src/main.tsx
+++ apps/admin-dashboard/src/main.tsx
@@ -1,22 +1,50 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
+import SignIn from "@/components/sign-in";
+import SignInLayout from "@/components/sign-in.layout";
 import Dashboard from "@/components/dashboard";
+import DashboardLayout from "@/components/dashboard.layout";
+
 import Inventory from "@/components/inventory";
-import Item from "@/components/item";
 
 import "./globals.css";
+import CheckEmail from "./components/check-email";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />
+      },
+      {
+        path: "inventory",
+        element: <Inventory />
+      }
+    ]
+  },
+  {
+    path: "/auth",
+    element: <SignInLayout />,
+    children: [
+      {
+        path: "signin",
+        element: <SignIn />
+      },
+      {
+        path: "check-email",
+        element: <CheckEmail />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/inventory" element={<Inventory />}>
-          <Route path=":id" element={<Item />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
