c262547568b9a25ce924eab261eb9163939d7a13
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-01-17 21:52:13 +0800

    wip: magic-link minor changes

--- apps/admin-express-api/src/strategies/magic-link.mjs
+++ apps/admin-express-api/src/strategies/magic-link.mjs
@@ -16,6 +16,58 @@ passport.deserializeUser(async (id, done) => {
   done(null, id);
 });
 
+async function sendEmailRequest(user, token) {
+  return new Promise((res, rej) => {
+    const link = `${process.env.BACKEND_URL}/api/auth/login/email/verify?token=${token}`;
+    try {
+      plunk.emails.send({
+        to: user.email,
+        subject: "Qwick Sign In",
+        body: link
+      });
+      res("email sent");
+    } catch (error) {
+      rej(error);
+    }
+  });
+}
+
+async function upsert(email) {
+  try {
+    const result = await User.findOneAndUpdate(
+      {
+        email,
+        "accounts.type": "email"
+      },
+      {
+        name: "",
+        email,
+        image: "",
+        verified: true,
+        accounts: [
+          {
+            type: "email",
+            provider: "magiclink",
+            accessToken: null,
+            refreshToken: null,
+            scope: null
+          }
+        ]
+      },
+      {
+        upsert: true,
+        new: true
+      }
+    );
+
+    return { id: result.id };
+  } catch (error) {}
+}
+
+async function verifyMagicLink(user) {
+  await upsert(user.email);
+}
+
 export default passport.use(
   new MagicLinkStrategy(
     {
@@ -24,50 +76,7 @@ export default passport.use(
       userFields: ["email"],
       verifyUserAfterToken: true
     },
-    async (user, token) => {
-      const verificationLink = `http://localhost:8080/api/auth/login/email/verify?token=${token}`;
-
-      return plunk.emails.send({
-        to: user.email,
-        subject: "Sign in to Custom App",
-        body: verificationLink
-      });
-    },
-    async user => {
-      try {
-        const result = await User.findOneAndUpdate(
-          {
-            email: user.email,
-            "accounts.type": "email"
-          },
-          {
-            name: "",
-            email: user.email,
-            image: "",
-            verified: true,
-            accounts: [
-              {
-                type: "email",
-                provider: "magiclink",
-                accessToken: null,
-                refreshToken: null,
-                scope: null
-              }
-            ]
-          },
-          {
-            upsert: true,
-            new: true
-          }
-        );
-
-        Logger(Namespace.AUTH, `user ${user.email} verified and upserted`);
-
-        return { id: result._id.toString() };
-      } catch (error) {
-        Logger(Namespace.AUTH, `unable verify and upsert user ${user.email}`);
-        throw error;
-      }
-    }
+    sendEmailRequest,
+    verifyMagicLink
   )
 );
