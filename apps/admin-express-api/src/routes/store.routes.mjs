df08b5bef9034bcf012d2f7fff186148e426a3bb
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-02-06 22:33:02 +0800

    refac: update store handler

--- apps/admin-express-api/src/routes/store.routes.mjs
+++ apps/admin-express-api/src/routes/store.routes.mjs
@@ -1,8 +1,7 @@
 import express from "express";
-import isAuthorized from "../middlewares/autorized.mjs";
 
-import storeService from "../mongoose/store.services.mjs";
-import propertiesService from "../mongoose/properties.services.mjs";
+import handler from "../handlers/store.handler.mjs";
+import isAuthorized from "../middlewares/autorized.mjs";
 
 const routes = express.Router({
   strict: true
@@ -10,54 +9,8 @@ const routes = express.Router({
 
 routes.use(isAuthorized);
 
-routes.get("/", async (request, response) => {
-  try {
-    const id = request.user.id;
-    const stores = await storeService.getStores(id);
+routes.get("/", handler.getStores);
 
-    return response.status(200).json({
-      success: true,
-      data: stores[0],
-      message: `all stores successfully fetched`
-    });
-  } catch (error) {
-    return response.status(500).json({
-      success: false,
-      data: undefined,
-      message: `Unable to fetch all stores`
-    });
-  }
-});
-
-routes.post("/create", async (request, response) => {
-  try {
-    const data = request.body;
-    const user = request.user.id;
-
-    const store = await storeService.createStore({
-      name: data.name,
-      owner: user
-    });
-
-    // set the default store id once created.
-    await propertiesService.updateUserProperties(user, {
-      store: {
-        default: store.id
-      }
-    });
-
-    return response.status(201).json({
-      success: true,
-      data: store,
-      message: `${data.name} store successfully created`
-    });
-  } catch (error) {
-    return response.status(500).json({
-      success: false,
-      data: undefined,
-      message: `Unable to create ${data.name} store`
-    });
-  }
-});
+routes.post("/create", handler.createStore);
 
 export default routes;
