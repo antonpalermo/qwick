c7dedebae0b73d007e9139050753094263b60123
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-02-06 22:36:04 +0800

    disabled x-powered-by

--- apps/admin-express-api/src/createServer.mjs
+++ apps/admin-express-api/src/createServer.mjs
@@ -24,6 +24,7 @@ export function createServer() {
   };
 
   app.set("trust proxy", true);
+  app.disable("x-powered-by");
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
