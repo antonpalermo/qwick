e84ec7f2ee84fd243b544a228473d9a5aef40069
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-02-07 22:32:43 +0800

    refac: update magic-link stategy

--- apps/admin-express-api/src/mongoose/user.services.mjs
+++ apps/admin-express-api/src/mongoose/user.services.mjs
@@ -1,15 +1,71 @@
 import User from "./schemas/user.mjs";
 
-import Logger, { Namespace } from "../utils/logger.mjs";
+import BackendError from "../utils/errors.mjs";
 
 export async function createUser(user) {
   try {
     const createdUser = await User.create(user);
     return createdUser;
   } catch (error) {
-    Logger(Namespace.DATABASE, "unable to create store " + store.name);
-    throw new Error(error);
+    throw new BackendError("database", "unable to create and insert " + user);
   }
 }
 
-export default { createUser };
+export async function getUser(id) {
+  try {
+    return await User.findById(id).select([
+      "_id",
+      "name",
+      "email",
+      "image",
+      "verified"
+    ]);
+  } catch (error) {
+    throw new BackendError(
+      "database",
+      "unable to perform query at the moment " + id
+    );
+  }
+}
+
+/**
+ * this function will perform upsert if no existing email
+ * is found.
+ * @param {email} email used to locate user data
+ */
+export async function getUserByEmail(email) {
+  try {
+    return await User.findOneAndUpdate(
+      {
+        email,
+        "accounts.type": "email"
+      },
+      {
+        name: "",
+        email,
+        image: "",
+        verified: true,
+        accounts: [
+          {
+            type: "email",
+            provider: "magiclink",
+            accessToken: null,
+            refreshToken: null,
+            scope: null
+          }
+        ]
+      },
+      {
+        upsert: true,
+        new: true
+      }
+    );
+  } catch (error) {
+    throw new BackendError(
+      "database",
+      "unable to perform query at the moment " + email
+    );
+  }
+}
+
+export default { createUser, getUser, getUserByEmail };
