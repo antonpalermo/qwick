107c8cbd34371d85f73509a04540202d2f7f7ec2
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-02-22 23:42:19 +0800

    refac: setup new nest and vite app

--- apps/web-dashboard/vite.config.ts
+++ apps/web-dashboard/vite.config.ts
@@ -1,31 +1,38 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/web-dashboard',
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  build: {
-    outDir: '../../dist/apps/web-dashboard',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/web-dashboard',
+    server: {
+      port: 4200,
+      host: 'localhost',
+      proxy: {
+        '/api': `${process.env.BACKEND_SERVICE ?? 'http://localhost:3000/api'}`,
+      },
     },
-  },
-});
+    preview: {
+      port: 4300,
+      host: 'localhost',
+    },
+    plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    build: {
+      outDir: '../../dist/apps/web-dashboard',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+  });
+};
