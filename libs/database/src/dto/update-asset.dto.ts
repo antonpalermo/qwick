74d38e2fb6cf1ffc38b73caee203e5dad70eff5d
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-02-24 20:19:41 +0800

    refac: removed legacy db libs

--- libs/database/src/dto/update-asset.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAssetDto } from './create-asset.dto';
-
-export class UpdateAssetDto extends PartialType(CreateAssetDto) {
-  id: number;
-}
--- libs/database/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './database.module';
-export * from './modules/asset.module';
-export * from './services/asset.service';
